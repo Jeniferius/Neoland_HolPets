6ef761473bbe712b3971a16cb7f96076c26197fb
DESKTOP-HPP1BS6\Jeniferius <jenifeirus018@gmail.com>
2018-08-20 15:31:41 +0200

    Comenzando a maquetar registroCuidador

--- src/app/registro-ser-cuidador/registro-ser-cuidador.component.ts
+++ src/app/registro-ser-cuidador/registro-ser-cuidador.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro-ser-cuidador',
@@ -6,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro-ser-cuidador.component.scss']
 })
 export class RegistroSerCuidadorComponent implements OnInit {
-
-  constructor() { }
+  formulario: FormGroup;
+  
+  constructor(private router: Router) { 
+    this.formulario = new FormGroup({
+      modalFormNameEx: new FormControl('', Validators.required),
+      modalFormEmailEx: new FormControl('', Validators.required),
+      modalFormSubjectEx: new FormControl('', Validators.required),
+      modalFormTextEx: new FormControl('', Validators.required),
+    });
+  }
 
   ngOnInit() {
   }
